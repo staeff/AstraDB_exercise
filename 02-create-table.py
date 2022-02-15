9c1becc4be893f409be544e75075514d39d561ed
Stephan Klinger <staeff@arcor.de>
2022-02-15 23:02:29 +0100

    Add create table exercise

--- 02-create-table.py
+++ 02-create-table.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 from connection import session
 
-print('========================================')
-print('Start exercise')
-try:
-    session.execute("""CREATE TABLE IF NOT EXISTS todoitems (
+CREATE_TABLE = """CREATE TABLE IF NOT EXISTS todoitems (
                         user_id         TEXT,
                         item_id         TIMEUUID,
                         title           TEXT,
                         completed       BOOLEAN,
                         PRIMARY KEY ((user_id), item_id)
                     ) WITH CLUSTERING ORDER BY (item_id ASC);
-                    """)
-except Exception as e:
-    print(e)
-    print('Failure')
-else:
-    print('Success')
-print('========================================')
+"""
+
+print('Creating table todoitems')
+session.execute(CREATE_TABLE)
